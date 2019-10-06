587a690f8c4b6f05b475b416b9e121ae8fe78dd5
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-10-06 19:08:43 +0300

    Update DetailsActivity to show information for clicked movie

--- app/src/main/java/dev/kirillzhelt/androidacademyapp/DetailsActivity.kt
+++ app/src/main/java/dev/kirillzhelt/androidacademyapp/DetailsActivity.kt
@@ -6,40 +6,46 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import dev.kirillzhelt.androidacademyapp.model.Movie
+import java.lang.IllegalArgumentException
 
 class DetailsActivity : AppCompatActivity() {
 
-    private lateinit var movieTrailerButton: Button
+    private lateinit var movie: Movie
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
-        movieTrailerButton = findViewById(R.id.btn_movie_trailer)
+        movie = intent?.getParcelableExtra(ARGS_MOVIE) ?: throw IllegalArgumentException("Missing movie argument")
 
-        movieTrailerButton.setOnClickListener { openMovieTrailer() }
+        findViewById<TextView>(R.id.details_movie_title_tv).text = movie.movieTitle
+        findViewById<TextView>(R.id.details_movie_description_tv).text = movie.movieDescription
+        findViewById<TextView>(R.id.details_movie_release_date_tv).text = movie.movieReleaseDate
+
+        findViewById<Button>(R.id.details_btn_movie_trailer).setOnClickListener {
+            openMovieTrailer()
+        }
+
+        Picasso.get().load(movie.moviePosterURL).placeholder(R.drawable.movie_poster_placeholder).into(findViewById<ImageView>(R.id.details_movie_poster_iv))
+        Picasso.get().load(movie.movieBackgroundURL).placeholder(R.drawable.movie_poster_placeholder).into(findViewById<ImageView>(R.id.details_movie_background_iv))
     }
 
     private fun openMovieTrailer() {
-        val appIntent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + TRAILER_YOUTUBE_ID))
-        val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=" + TRAILER_YOUTUBE_ID))
+        val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(movie.movieTrailerURL))
 
-        if (appIntent.resolveActivity(packageManager) != null) {
-            startActivity(appIntent)
-        } else {
+        if (webIntent.resolveActivity(packageManager) != null)
             startActivity(webIntent)
-        }
-
     }
 
     companion object {
-        const val TRAILER_YOUTUBE_ID = "6ZfuNTqbHE8"
         const val ARGS_MOVIE = "ARGS_MOVIE"
 
         fun createIntent(context: Context, movie: Movie) : Intent {
-            val intent = Intent(context, this::class.java)
+            val intent = Intent(context, DetailsActivity::class.java)
             intent.putExtra(ARGS_MOVIE, movie)
 
             return intent
