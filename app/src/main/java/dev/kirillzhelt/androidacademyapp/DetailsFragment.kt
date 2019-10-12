522f3f428c99b3545a30b28260ffc450c6d9cdde
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-10-12 17:41:53 +0300

    Pass movie to DetailsFragment

--- app/src/main/java/dev/kirillzhelt/androidacademyapp/DetailsFragment.kt
+++ app/src/main/java/dev/kirillzhelt/androidacademyapp/DetailsFragment.kt
@@ -5,21 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import dev.kirillzhelt.androidacademyapp.model.Movie
 
 class DetailsFragment : Fragment() {
 
-//    companion object {
-//
-//        fun newInstance() {
-//            val instance = DetailsFragment()
-//
-//            val bundle = Bundle().apply {
-//                putParcelableArrayList()
-//            }
-//
-//        }
-//
-//    }
+    companion object {
+        private const val ARG_MOVIE = "ARG_MOVIE"
+
+        fun newInstance(movie: Movie): DetailsFragment {
+            val instance = DetailsFragment()
+
+            val bundle = Bundle().apply {
+                putParcelable(ARG_MOVIE, movie)
+            }
+
+            instance.arguments = bundle
+
+            return instance
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
--- app/src/main/java/dev/kirillzhelt/androidacademyapp/adapters/DetailsSlidePagerAdapter.kt
+++ app/src/main/java/dev/kirillzhelt/androidacademyapp/adapters/DetailsSlidePagerAdapter.kt
@@ -9,7 +9,7 @@ import dev.kirillzhelt.androidacademyapp.model.Movie
 class DetailsSlidePagerAdapter(fragmentManager: FragmentManager, private val movies: ArrayList<Movie>) : FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     override fun getItem(position: Int): Fragment {
-        return DetailsFragment()
+        return DetailsFragment.newInstance(movies[position])
     }
 
     override fun getCount(): Int {
