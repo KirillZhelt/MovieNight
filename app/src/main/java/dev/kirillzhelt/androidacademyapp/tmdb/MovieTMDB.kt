9efaacca28bfd7302213c6fb4cadbafc40759bd6
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-03 18:07:54 +0300

    Create MovieTMDB class for storing data from JSON

---
+++ app/src/main/java/dev/kirillzhelt/androidacademyapp/tmdb/MovieTMDB.kt
@@ -0,0 +1,22 @@
+package dev.kirillzhelt.androidacademyapp.tmdb
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class MovieTMDB(
+    @Json(name = "poster_path")
+    val moviePosterURL: String,
+
+    @Json(name = "backdrop_path")
+    val movieBackgroundURL: String,
+
+    @Json(name = "title")
+    val movieTitle: String,
+
+    @Json(name = "overview")
+    val movieDescription: String,
+
+    @Json(name = "release_date")
+    val movieReleaseDate: String
+)
---
+++ app/src/main/java/dev/kirillzhelt/androidacademyapp/tmdb/TMDBService.kt
@@ -0,0 +1,7 @@
+package dev.kirillzhelt.androidacademyapp.tmdb
+
+interface TMDBService {
+
+
+
+}
