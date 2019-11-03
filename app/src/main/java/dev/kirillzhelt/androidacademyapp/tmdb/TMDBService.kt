e77a744c89bb0ace0044d4900f10927fcbb4400c
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-03 18:13:19 +0300

    Create TMDBService interface to make http request for retrieving movies

--- app/src/main/java/dev/kirillzhelt/androidacademyapp/tmdb/TMDBService.kt
+++ app/src/main/java/dev/kirillzhelt/androidacademyapp/tmdb/TMDBService.kt
@@ -1,7 +1,12 @@
 package dev.kirillzhelt.androidacademyapp.tmdb
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface TMDBService {
 
-
+    @GET("movie/popular")
+    fun getMovies(@Query("api_key") apiKey: String): Call<List<MovieTMDB>>
 
 }
