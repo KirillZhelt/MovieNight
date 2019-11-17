aed5fef4aeb9cdcf41d960dd13de32fb3a2e824c
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-17 20:10:55 +0300

    Create BackgroundProgressReceiver

---
+++ app/src/main/java/dev/kirillzhelt/androidacademyapp/services/BackgroundProgressReceiver.kt
@@ -0,0 +1,22 @@
+package dev.kirillzhelt.androidacademyapp.services
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BackgroundProgressReceiver(val listener: (Int) -> Unit): BroadcastReceiver() {
+
+    companion object {
+        const val PROGRESS_UPDATE_ACTION = "PROGRESS_UPDATE_ACTION"
+
+        const val PROGRESS_VALUE_KEY = "PROGRESS_VALUE_KEY"
+
+        const val PROGRESS_STATUS = "PROGRESS_STATUS"
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val progress = intent?.getIntExtra(PROGRESS_VALUE_KEY, -1) ?: -1
+
+        listener(progress)
+    }
+}
--- app/src/main/java/dev/kirillzhelt/androidacademyapp/services/ServiceActivity.kt
+++ app/src/main/java/dev/kirillzhelt/androidacademyapp/services/ServiceActivity.kt
@@ -1,13 +1,55 @@
 package dev.kirillzhelt.androidacademyapp.services
 
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import dev.kirillzhelt.androidacademyapp.R
 
 class ServiceActivity : AppCompatActivity() {
 
+    private lateinit var progressTextView: TextView
+
+    private var backgroundProgressReceiver: BackgroundProgressReceiver? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service)
+
+        progressTextView = findViewById(R.id.activity_service_progress_tv)
+
+        findViewById<Button>(R.id.activity_service_start_intent_service_btn).setOnClickListener {
+
+        }
+
+        findViewById<Button>(R.id.activity_service_start_service_btn).setOnClickListener {
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        subscribeForProgressUpdates()
+    }
+
+    override fun onPause() {
+        if (backgroundProgressReceiver != null) {
+            unregisterReceiver(backgroundProgressReceiver)
+        }
+
+        super.onPause()
+    }
+
+    private fun subscribeForProgressUpdates() {
+        if (backgroundProgressReceiver == null) {
+            backgroundProgressReceiver = BackgroundProgressReceiver { progress ->
+                progressTextView.text = progress.toString()
+            }
+        }
+
+        val progressUpdateActionFilter = IntentFilter(BackgroundProgressReceiver.PROGRESS_UPDATE_ACTION)
+        registerReceiver(backgroundProgressReceiver, progressUpdateActionFilter)
     }
 }
